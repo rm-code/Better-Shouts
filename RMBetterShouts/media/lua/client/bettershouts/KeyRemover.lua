c5aaf903035e0e2b5eafac6e811fef1e5f8733b1
Robert Machmer <robert.machmer@gmail.com>
2015-07-18 01:25:34 +0200

    Remove the vanilla key binding for shouting
    This is achieved by simply deleting the values from the keyBinding table
    when the game is booted.

---
+++ RMBetterShouts/media/lua/client/bettershouts/KeyRemover.lua
@@ -0,0 +1,26 @@
+require('keyBinding.lua');
+
+local KEYS_TO_REMOVE = {
+    'Shout',
+    '[NPC Interaction]',
+}
+
+---
+-- This function iterates over the table of keys we want to remove. For each
+-- of these values it will iterate over the keyBinding table and check if a
+-- value can be found. When a value exists in the keyBinding table it will be
+-- removed (all indexes will be shifted down accordingly by table.remove) and
+-- the function breaks out of the nested loop.
+--
+local function removeKeys()
+    for i = 1, #KEYS_TO_REMOVE do
+        for j = 1, #keyBinding do
+            if keyBinding[j].value == KEYS_TO_REMOVE[i] then
+                table.remove(keyBinding, j);
+                break;
+            end
+        end
+    end
+end
+
+Events.OnGameBoot.Add(removeKeys);
