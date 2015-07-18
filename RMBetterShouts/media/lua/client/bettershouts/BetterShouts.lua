43454f43405ed3fb8f5476fdaee23222f31e618c
Robert Machmer <robert.machmer@gmail.com>
2015-07-18 21:07:54 +0200

    Add a bunch of different strings for shouting use them randomly

--- RMBetterShouts/media/lua/client/bettershouts/BetterShouts.lua
+++ RMBetterShouts/media/lua/client/bettershouts/BetterShouts.lua
@@ -4,13 +4,32 @@ local VOLUME_WHISPER = 20;
 local RADIUS_SHOUT = 30;
 local VOLUME_SHOUT = 50;
 
+local SHOUTS = {
+    'Over here!',
+    'Hey, over here!',
+    'Hey maggot-heads!',
+    'Are you hungry?',
+    'Get over here!',
+    'This is my town now!',
+    'Oi, you smelly scumbags!',
+    'Catch me if you can!',
+};
+
+local WHISPERS = {
+    'Psssssst... over here!',
+    'Hey, yes you. Come here!',
+    'Get some tasty meat. Just come to me!',
+    'Yes, come closer.',
+    'Just a little closer ...',
+};
+
 local function shout()
     local player = getPlayer();
     if player and player:IsSneaking() then
-        player:Say('Psssst! Over here!');
+        player:Say(WHISPERS[ZombRand(#WHISPERS) + 1]);
         addSound(player, player:getX(), player:getY(), player:getZ(), RADIUS_WHISPER, VOLUME_WHISPER);
     elseif player then
-        player:Say('Supercalifragilisticexpialidocious!');
+        player:Say(SHOUTS[ZombRand(#SHOUTS) + 1]);
         addSound(player, player:getX(), player:getY(), player:getZ(), RADIUS_SHOUT, VOLUME_SHOUT);
     end
 end
