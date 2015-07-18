3ce741840295556b765ae8ca178507076763046b
Robert Machmer <robert.machmer@gmail.com>
2015-07-18 10:07:39 +0200

    Add a different shouting mechanic for when the player is sneaking
    This could be used to only attract zombies in a short radius.

--- RMBetterShouts/media/lua/client/bettershouts/BetterShouts.lua
+++ RMBetterShouts/media/lua/client/bettershouts/BetterShouts.lua
@@ -1,9 +1,15 @@
+local RADIUS_WHISPER = 10;
+local VOLUME_WHISPER = 20;
+
 local RADIUS_SHOUT = 20;
 local VOLUME_SHOUT = 50;
 
 local function shout()
     local player = getPlayer();
-    if player then
+    if player and player:IsSneaking() then
+        player:Say('Psssst! Over here!');
+        addSound(player, player:getX(), player:getY(), player:getZ(), RADIUS_WHISPER, VOLUME_WHISPER);
+    elseif player then
         player:Say('Supercalifragilisticexpialidocious!');
         addSound(player, player:getX(), player:getY(), player:getZ(), RADIUS_SHOUT, VOLUME_SHOUT);
     end
